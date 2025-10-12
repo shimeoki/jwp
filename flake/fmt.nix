c435540f56a5c3b5b653dd248aef7bee57f327f4
shimeoki <shimeoki@gmail.com>
2025-10-12 12:24:33 +0300

    feat(flake): use treefmt and split modules

---
+++ flake/fmt.nix
@@ -0,0 +1,28 @@
+{ inputs, ... }:
+{
+    imports = [
+        inputs.treefmt.flakeModule
+    ];
+
+    perSystem = {
+        treefmt = {
+            programs = {
+                # keep-sorted start block=yes newline_separated=yes
+                keep-sorted = {
+                    enable = true;
+                };
+
+                nixfmt = {
+                    enable = true;
+                    width = 80;
+                };
+                # keep-sorted end
+            };
+
+            settings.formatter = {
+                # TODO: use indent option after numtide/treefmt-nix#416
+                nixfmt.options = [ "--indent=4" ];
+            };
+        };
+    };
+}
