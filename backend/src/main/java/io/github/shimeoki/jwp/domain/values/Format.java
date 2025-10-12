e5dd9a0385af570c1a93afbd947005bf3374d0d6
shimeoki <shimeoki@gmail.com>
2025-10-12 13:52:59 +0300

    feat: Format enum

---
+++ backend/src/main/java/io/github/shimeoki/jwp/domain/values/Format.java
@@ -0,0 +1,29 @@
+package io.github.shimeoki.jwp.domain.values;
+
+public enum Format {
+
+    JPEG("jpg"),
+    PNG("png");
+
+    private final String value;
+
+    private Format(final String value) {
+        this.value = value;
+    }
+
+    public static Format fromExtension(final String extension) {
+        switch (extension.toLowerCase()) {
+            case "jpg", "jpeg":
+                return JPEG;
+            case "png":
+                return PNG;
+            default:
+                throw new IllegalArgumentException("invalid extension");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return this.value;
+    }
+}
