fff3c162f3e0bb95ed686ba521b19b59d02d38cb
shimeoki <shimeoki@gmail.com>
2025-10-18 18:43:50 +0300

    feat(flake): app

--- flake.nix
+++ flake.nix
@@ -22,6 +22,7 @@
 
             imports = [
                 # keep-sorted start
+                ./flake/app.nix
                 ./flake/dev.nix
                 ./flake/fmt.nix
                 ./flake/package.nix
