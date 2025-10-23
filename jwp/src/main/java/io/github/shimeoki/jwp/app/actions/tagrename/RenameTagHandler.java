c102208e90bba1043ca6aaa4c6ed7c684280148e
shimeoki <shimeoki@gmail.com>
2025-10-23 10:32:41 +0300

    feat(tagrename): provider + worker

--- jwp/src/main/java/io/github/shimeoki/jwp/app/actions/tagrename/RenameTagHandler.java
+++ jwp/src/main/java/io/github/shimeoki/jwp/app/actions/tagrename/RenameTagHandler.java
@@ -3,47 +3,50 @@ package io.github.shimeoki.jwp.app.actions.tagrename;
 import java.util.Objects;
 
 import io.github.shimeoki.jwp.app.Handler;
-import io.github.shimeoki.jwp.domain.repositories.TagRepository;
-import io.github.shimeoki.jwp.domain.repositories.WallpaperRepository;
 import io.github.shimeoki.jwp.domain.values.Name;
 
 public final class RenameTagHandler
         implements Handler<RenameTagCommand, RenameTagResult> {
 
-    private final WallpaperRepository wallpapers;
-    private final TagRepository tags;
+    private final RenameTagWorker worker;
 
-    public RenameTagHandler(
-            final WallpaperRepository wallpapers,
-            final TagRepository tags) {
-
-        this.wallpapers = Objects.requireNonNull(wallpapers);
-        this.tags = Objects.requireNonNull(tags);
+    public RenameTagHandler(final RenameTagWorker w) {
+        worker = Objects.requireNonNull(w);
     }
 
     @Override
     public RenameTagResult handle(final RenameTagCommand cmd) {
-        final var before = new Name(cmd.before());
-        final var tag = tags.findByName(before).orElseThrow(
-                () -> new IllegalArgumentException("tag not found"));
+        try (final var p = worker.work()) {
+            final var tags = p.tagRepository();
+            final var wallpapers = p.wallpaperRepository();
 
-        final var after = new Name(cmd.after());
-        tag.rename(after);
+            final var before = new Name(cmd.before());
+            final var tag = tags.findByName(before).orElseThrow(
+                    () -> new IllegalArgumentException("tag not found"));
 
-        tags.findByName(after).ifPresent((existent) -> {
-            final var id = existent.id();
-            final var walls = wallpapers.findByTagID(id);
+            final var after = new Name(cmd.after());
+            tag.rename(after);
 
-            walls.forEach((wall) -> {
-                wall.removeTag(id);
-                wall.addTag(tag);
-                wallpapers.save(wall);
+            tags.findByName(after).ifPresent((existent) -> {
+                final var id = existent.id();
+                final var walls = wallpapers.findByTagID(id);
+
+                walls.forEach((wall) -> {
+                    wall.removeTag(id);
+                    wall.addTag(tag);
+                    wallpapers.save(wall);
+                });
+
+                tags.delete(id);
             });
 
-            tags.delete(id);
-        });
+            tags.save(tag);
+            p.commit();
 
-        tags.save(tag);
-        return new RenameTagResult();
+            return new RenameTagResult();
+        } catch (final Exception e) {
+            // TODO: handle
+            return null;
+        }
     }
 }
