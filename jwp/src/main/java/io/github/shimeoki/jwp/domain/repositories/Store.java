525d10cf437dfd616e637a0e48085a6f10cd6655
shimeoki <shimeoki@gmail.com>
2025-10-23 21:48:44 +0300

    feat(Store): add IOException

--- jwp/src/main/java/io/github/shimeoki/jwp/domain/repositories/Store.java
+++ jwp/src/main/java/io/github/shimeoki/jwp/domain/repositories/Store.java
@@ -1,14 +1,15 @@
 package io.github.shimeoki.jwp.domain.repositories;
 
+import java.io.IOException;
 import java.io.InputStream;
 
 import io.github.shimeoki.jwp.domain.values.Hash;
 
 public interface Store {
 
-    InputStream get(Hash h);
+    InputStream get(Hash h) throws IOException;
 
-    Hash create(InputStream img);
+    Hash create(InputStream img) throws IOException;
 
-    void delete(Hash h);
+    void delete(Hash h) throws IOException;
 }
