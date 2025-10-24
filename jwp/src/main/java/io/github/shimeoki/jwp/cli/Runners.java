a9e8f2463c3bc28cdb9da2fb057312d33670aefd
shimeoki <shimeoki@gmail.com>
2025-10-24 14:54:12 +0300

    fix(Runners): check args in exactArgs

--- jwp/src/main/java/io/github/shimeoki/jwp/cli/Runners.java
+++ jwp/src/main/java/io/github/shimeoki/jwp/cli/Runners.java
@@ -1,8 +1,16 @@
 package io.github.shimeoki.jwp.cli;
 
+import java.util.Objects;
+
 public final class Runners {
 
     public static Runner exactArgs(final int n, final Runner r) {
+        Objects.requireNonNull(r);
+
+        if (n < 0) {
+            throw new IllegalArgumentException("invalid length");
+        }
+
         return (cmd, args) -> {
             if (args.length != n) {
                 throw new IllegalArgumentException(String.format(
