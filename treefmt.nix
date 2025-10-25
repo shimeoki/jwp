3f3674d8d39a1598c9a061b1ba14444a178a6590
shimeoki <shimeoki@gmail.com>
2025-10-25 19:16:54 +0300

    refactor(flake): treefmt.nix in the root

---
+++ treefmt.nix
@@ -0,0 +1,25 @@
+# TODO: java
+
+{
+    programs = {
+        # keep-sorted start block=yes newline_separated=yes
+        deno = {
+            enable = true;
+            includes = [
+                "deno.json"
+                "README.md"
+            ];
+        };
+
+        keep-sorted = {
+            enable = true;
+        };
+
+        nixfmt = {
+            enable = true;
+            width = 80;
+            indent = 4;
+        };
+        # keep-sorted end
+    };
+}
