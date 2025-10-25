b6bb2ba1f810466b6af0c25a731fd1be24019eac
shimeoki <shimeoki@gmail.com>
2025-10-25 09:47:07 +0300

    refactor(flake): shorten fmt block

--- flake/fmt.nix
+++ flake/fmt.nix
@@ -4,29 +4,29 @@
         inputs.treefmt.flakeModule
     ];
 
+    # TODO: java
+
     perSystem = {
-        treefmt = {
-            programs = {
-                # keep-sorted start block=yes newline_separated=yes
-                deno = {
-                    enable = true;
-                    includes = [
-                        "deno.json"
-                        "README.md"
-                    ];
-                };
-
-                keep-sorted = {
-                    enable = true;
-                };
-
-                nixfmt = {
-                    enable = true;
-                    width = 80;
-                    indent = 4;
-                };
-                # keep-sorted end
+        treefmt.programs = {
+            # keep-sorted start block=yes newline_separated=yes
+            deno = {
+                enable = true;
+                includes = [
+                    "deno.json"
+                    "README.md"
+                ];
             };
+
+            keep-sorted = {
+                enable = true;
+            };
+
+            nixfmt = {
+                enable = true;
+                width = 80;
+                indent = 4;
+            };
+            # keep-sorted end
         };
     };
 }
