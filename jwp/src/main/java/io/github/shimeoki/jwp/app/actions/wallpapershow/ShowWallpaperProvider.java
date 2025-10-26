121916704e683a4b6b36d6daa042ca717fe452c7
shimeoki <shimeoki@gmail.com>
2025-10-26 17:24:31 +0300

    feat(app): show wallpaper qry

---
+++ jwp/src/main/java/io/github/shimeoki/jwp/app/actions/wallpapershow/ShowWallpaperProvider.java
@@ -0,0 +1,8 @@
+package io.github.shimeoki.jwp.app.actions.wallpapershow;
+
+import io.github.shimeoki.jwp.app.providers.StoreProvider;
+import io.github.shimeoki.jwp.app.providers.WallpaperProvider;
+
+public interface ShowWallpaperProvider
+        extends StoreProvider, WallpaperProvider {
+}
