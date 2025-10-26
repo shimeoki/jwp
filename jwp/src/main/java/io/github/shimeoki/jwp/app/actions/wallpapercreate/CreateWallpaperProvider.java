3f4672cd45e352954a8e18786cdad6788e144cd2
shimeoki <shimeoki@gmail.com>
2025-10-26 17:03:03 +0300

    feat(app): wallpaper create cmd

---
+++ jwp/src/main/java/io/github/shimeoki/jwp/app/actions/wallpapercreate/CreateWallpaperProvider.java
@@ -0,0 +1,8 @@
+package io.github.shimeoki.jwp.app.actions.wallpapercreate;
+
+import io.github.shimeoki.jwp.app.providers.StoreProvider;
+import io.github.shimeoki.jwp.app.providers.WallpaperProvider;
+
+public interface CreateWallpaperProvider
+        extends StoreProvider, WallpaperProvider {
+}
---
+++ jwp/src/main/java/io/github/shimeoki/jwp/app/actions/wallpapercreate/CreateWallpaperWorker.java
@@ -0,0 +1,6 @@
+package io.github.shimeoki.jwp.app.actions.wallpapercreate;
+
+import io.github.shimeoki.jwp.app.Worker;
+
+public interface CreateWallpaperWorker extends Worker<CreateWallpaperProvider> {
+}
