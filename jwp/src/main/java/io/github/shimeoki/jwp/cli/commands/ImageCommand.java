38438fd2442ce05ab76e51e6de30a01593da3319
shimeoki <shimeoki@gmail.com>
2025-10-26 17:37:31 +0300

    feat(cli): image cmd

---
+++ jwp/src/main/java/io/github/shimeoki/jwp/cli/commands/ImageCommand.java
@@ -0,0 +1,21 @@
+package io.github.shimeoki.jwp.cli.commands;
+
+import io.github.shimeoki.jwp.cli.Command;
+import io.github.shimeoki.jwp.config.App;
+
+public final class ImageCommand extends Command {
+
+    public ImageCommand(final App app) {
+        super("image");
+    }
+
+    @Override
+    public void run(final String[] args) {
+        System.out.println(help());
+    }
+
+    @Override
+    public String description() {
+        return "Wallpaper and image related commands";
+    }
+}
