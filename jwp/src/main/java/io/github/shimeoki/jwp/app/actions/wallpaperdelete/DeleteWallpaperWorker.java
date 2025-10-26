f2dd77cb65ecfd3ee8e5b340786da83cab97f874
shimeoki <shimeoki@gmail.com>
2025-10-26 17:09:39 +0300

    feat(app): delete wallpaper cmd

---
+++ jwp/src/main/java/io/github/shimeoki/jwp/app/actions/wallpaperdelete/DeleteWallpaperWorker.java
@@ -0,0 +1,6 @@
+package io.github.shimeoki.jwp.app.actions.wallpaperdelete;
+
+import io.github.shimeoki.jwp.app.Worker;
+
+public interface DeleteWallpaperWorker extends Worker<DeleteWallpaperProvider> {
+}
