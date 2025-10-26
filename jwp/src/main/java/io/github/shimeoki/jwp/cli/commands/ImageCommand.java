4f803338160cd23f4529ffb2d93ccdac0029aa59
shimeoki <shimeoki@gmail.com>
2025-10-26 17:57:01 +0300

    feat(cli): add current image commands

--- jwp/src/main/java/io/github/shimeoki/jwp/cli/commands/ImageCommand.java
+++ jwp/src/main/java/io/github/shimeoki/jwp/cli/commands/ImageCommand.java
@@ -1,12 +1,14 @@
 package io.github.shimeoki.jwp.cli.commands;
 
 import io.github.shimeoki.jwp.cli.Command;
+import io.github.shimeoki.jwp.cli.commands.image.CreateCommand;
 import io.github.shimeoki.jwp.config.App;
 
 public final class ImageCommand extends Command {
 
     public ImageCommand(final App app) {
         super("image");
+        addCommand(new CreateCommand(app));
     }
 
     @Override
