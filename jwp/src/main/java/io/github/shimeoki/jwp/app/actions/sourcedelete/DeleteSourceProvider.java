5fa1877e2b55ee83112597695fc1d45f5d497e6d
shimeoki <shimeoki@gmail.com>
2025-10-26 20:48:40 +0300

    feat(app): source delete cmd

---
+++ jwp/src/main/java/io/github/shimeoki/jwp/app/actions/sourcedelete/DeleteSourceProvider.java
@@ -0,0 +1,6 @@
+package io.github.shimeoki.jwp.app.actions.sourcedelete;
+
+import io.github.shimeoki.jwp.app.providers.SourceProvider;
+
+public interface DeleteSourceProvider extends SourceProvider {
+}
