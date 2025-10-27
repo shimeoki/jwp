cedb80b6d91459bc3b8d8c3edb4457114375f5db
shimeoki <shimeoki@gmail.com>
2025-10-27 17:21:05 +0300

    feat(app): tag add cmd

---
+++ jwp/src/main/java/io/github/shimeoki/jwp/app/actions/tagadd/AddTagProvider.java
@@ -0,0 +1,7 @@
+package io.github.shimeoki.jwp.app.actions.tagadd;
+
+import io.github.shimeoki.jwp.app.providers.TagProvider;
+import io.github.shimeoki.jwp.app.providers.WallpaperProvider;
+
+public interface AddTagProvider extends WallpaperProvider, TagProvider {
+}
---
+++ jwp/src/main/java/io/github/shimeoki/jwp/app/actions/tagadd/AddTagWorker.java
@@ -0,0 +1,6 @@
+package io.github.shimeoki.jwp.app.actions.tagadd;
+
+import io.github.shimeoki.jwp.app.Worker;
+
+public interface AddTagWorker extends Worker<AddTagProvider> {
+}
