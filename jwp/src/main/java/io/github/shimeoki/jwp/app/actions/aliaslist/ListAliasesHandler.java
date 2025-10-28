0cb51db73d6bcbaf513e1d14790cab2c92bf5737
shimeoki <shimeoki@gmail.com>
2025-10-28 14:04:02 +0300

    feat(aliaslist): print hash instead of id
    because the user interacts with the hash, not id

--- jwp/src/main/java/io/github/shimeoki/jwp/app/actions/aliaslist/ListAliasesHandler.java
+++ jwp/src/main/java/io/github/shimeoki/jwp/app/actions/aliaslist/ListAliasesHandler.java
@@ -16,10 +16,20 @@ public final class ListAliasesHandler
     @Override
     public ListAliasesResult handle(final ListAliasesQuery qry) {
         try (final var p = worker.work()) {
-            return new ListAliasesResult(p.aliasRepository().findAll().map(
-                    (a) -> String.format("%s -> %s",
-                            a.name().toString(), a.wallpaperID().toString()))
-                    .toArray(String[]::new));
+            final var aliases = p.aliasRepository().findAll();
+
+            final var lines = aliases.map((a) -> {
+                final var wid = a.wallpaperID();
+
+                final var w = p.wallpaperRepository().findByID(wid).orElseThrow(
+                        () -> new IllegalStateException("invalid wallpaper id"));
+
+                return String.format("%s -> %s",
+                        a.name().toString(),
+                        w.hash().toString());
+            }).toArray(String[]::new);
+
+            return new ListAliasesResult(lines);
         } catch (final Exception e) {
             // TODO: handle
             return null;
--- jwp/src/main/java/io/github/shimeoki/jwp/app/actions/aliaslist/ListAliasesProvider.java
+++ jwp/src/main/java/io/github/shimeoki/jwp/app/actions/aliaslist/ListAliasesProvider.java
@@ -1,6 +1,7 @@
 package io.github.shimeoki.jwp.app.actions.aliaslist;
 
 import io.github.shimeoki.jwp.app.providers.AliasProvider;
+import io.github.shimeoki.jwp.app.providers.WallpaperProvider;
 
-public interface ListAliasesProvider extends AliasProvider {
+public interface ListAliasesProvider extends AliasProvider, WallpaperProvider {
 }
